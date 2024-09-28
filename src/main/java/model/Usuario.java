1eaa7e91e5c86c8c44ff801ceb8ee671f9e1c338
João Paulo <joao.santos1136@etec.sp.gov.br>
2024-09-27 23:48:13 -0300

    Create entidade usuario

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,63 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "login")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "username", unique = true) 
+    private String userName;
+    @Column(name = "senha")
+    private String senha;
+
+    public Usuario(String userName, String senha) {
+        this.userName = userName;
+        this.senha = senha;
+    }
+
+    public void cadastrar(String nome, String userName, String senha) {
+        this.userName= userName;
+        this.senha = senha;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+}
