05c2181b90d6cb1a67269a5e4cbed8e009ceda52
João Paulo <joao.santos1136@etec.sp.gov.br>
2024-09-29 16:45:28 -0300

    create flat

--- src/main/java/pim/estufapim/Main.java
+++ src/main/java/pim/estufapim/Main.java
@@ -1,10 +1,12 @@
 package pim.estufapim;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import dao.EstufaDAO;
 import view.CadastroView;
 import controller.ControllerCadastro;
 import controller.ControllerCadastroUsuario;
 import controller.ControllerLoginUsuario;
+import javax.swing.UIManager;
 import view.CadastroLoginUsuarioView;
 import view.CadastroUsuarioView;
 
@@ -17,6 +19,15 @@ EstufaDAO estufaDAO = new EstufaDAO();
 ControllerCadastro controllerCadastro = new ControllerCadastro(cadastroView, estufaDAO);
 cadastroView.setVisible(true);
          */
+        flat();
         ControllerLoginUsuario usuario = new ControllerLoginUsuario(new CadastroLoginUsuarioView(null, true));
+    }
+
+    public static void flat() {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
     }
 }
