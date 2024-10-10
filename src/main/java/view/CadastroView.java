d52e318d55972c3dfcee557e98532b935320392d
João Paulo <joao.santos1136@etec.sp.gov.br>
2024-10-09 23:22:47 -0300

    design view cadastro estufa

--- src/main/java/view/CadastroView.java
+++ src/main/java/view/CadastroView.java
@@ -30,137 +30,148 @@ public class CadastroView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        lblNameEstufa = new javax.swing.JLabel();
-        lblNamePlanta = new javax.swing.JLabel();
+        sliderUmiMinima = new javax.swing.JSlider();
         lblTempMax = new javax.swing.JLabel();
-        lblTempMin = new javax.swing.JLabel();
-        lblUmiMax = new javax.swing.JLabel();
-        lblUmiMin = new javax.swing.JLabel();
-        lblQtdLumens = new javax.swing.JLabel();
+        sliderQuantLumens = new javax.swing.JSlider();
         nomeEstufaField = new javax.swing.JTextField();
+        lblUmiMax = new javax.swing.JLabel();
         nomePlantaField = new javax.swing.JTextField();
         lumensField = new javax.swing.JTextField();
-        temperaturaMaxField = new javax.swing.JTextField();
-        temperaturaMinField = new javax.swing.JTextField();
+        salvarButton = new javax.swing.JButton();
+        lblQtdLumens = new javax.swing.JLabel();
+        lblNameEstufa = new javax.swing.JLabel();
+        lblNamePlanta = new javax.swing.JLabel();
         umidadeMaxField = new javax.swing.JTextField();
         umidadeMinField = new javax.swing.JTextField();
-        salvarButton = new javax.swing.JButton();
-        sliderQuantLumens = new javax.swing.JSlider();
-        sliderTempMaxima = new javax.swing.JSlider();
         sliderTempMinima = new javax.swing.JSlider();
+        temperaturaMinField = new javax.swing.JTextField();
+        lblUmiMin = new javax.swing.JLabel();
+        temperaturaMaxField = new javax.swing.JTextField();
+        sliderTempMaxima = new javax.swing.JSlider();
+        lblTempMin = new javax.swing.JLabel();
         sliderUmiMaxima = new javax.swing.JSlider();
-        sliderUmiMinima = new javax.swing.JSlider();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Cadastro de Estufas");
-        setMinimumSize(new java.awt.Dimension(900, 500));
-        setSize(new java.awt.Dimension(900, 500));
+        setMaximumSize(new java.awt.Dimension(914, 514));
+        setMinimumSize(new java.awt.Dimension(914, 514));
+        setPreferredSize(new java.awt.Dimension(914, 514));
+        setSize(new java.awt.Dimension(914, 514));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Cadastro de Estufa", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Verdana", 1, 11))); // NOI18N
-        jPanel1.setFont(new java.awt.Font("Verdana", 0, 11)); // NOI18N
-        jPanel1.setMaximumSize(new java.awt.Dimension(876, 453));
-        jPanel1.setMinimumSize(new java.awt.Dimension(876, 453));
-        jPanel1.setLayout(null);
+        sliderUmiMinima.setForeground(new java.awt.Color(94, 219, 18));
+        getContentPane().add(sliderUmiMinima, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 270, -1, -1));
 
-        lblNameEstufa.setText("Nome da Estufa:");
-        jPanel1.add(lblNameEstufa);
-        lblNameEstufa.setBounds(17, 48, 87, 16);
-
-        lblNamePlanta.setText("Nome da Planta:");
-        jPanel1.add(lblNamePlanta);
-        lblNamePlanta.setBounds(340, 50, 88, 16);
-
+        lblTempMax.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        lblTempMax.setForeground(new java.awt.Color(255, 255, 255));
         lblTempMax.setText("Temperatura Máxima:");
-        jPanel1.add(lblTempMax);
-        lblTempMax.setBounds(17, 150, 116, 16);
+        getContentPane().add(lblTempMax, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, -1, -1));
 
-        lblTempMin.setText("Temperatura Mínima:");
-        jPanel1.add(lblTempMin);
-        lblTempMin.setBounds(450, 150, 114, 16);
+        sliderQuantLumens.setForeground(new java.awt.Color(94, 219, 18));
+        getContentPane().add(sliderQuantLumens, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 360, -1, -1));
 
-        lblUmiMax.setText("Umidade Máxima:");
-        jPanel1.add(lblUmiMax);
-        lblUmiMax.setBounds(17, 250, 97, 16);
-
-        lblUmiMin.setText("Umidade Mínima:");
-        jPanel1.add(lblUmiMin);
-        lblUmiMin.setBounds(440, 260, 95, 16);
-
-        lblQtdLumens.setText("Quantidade de Lumens:");
-        jPanel1.add(lblQtdLumens);
-        lblQtdLumens.setBounds(17, 345, 126, 16);
-        jPanel1.add(nomeEstufaField);
-        nomeEstufaField.setBounds(120, 47, 140, 22);
-
-        nomePlantaField.addActionListener(new java.awt.event.ActionListener() {
+        nomeEstufaField.setBackground(new java.awt.Color(204, 204, 204));
+        nomeEstufaField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                nomePlantaFieldActionPerformed(evt);
+                nomeEstufaFieldActionPerformed(evt);
             }
         });
-        jPanel1.add(nomePlantaField);
-        nomePlantaField.setBounds(440, 47, 140, 22);
+        getContentPane().add(nomeEstufaField, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, 250, -1));
 
+        lblUmiMax.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        lblUmiMax.setForeground(new java.awt.Color(255, 255, 255));
+        lblUmiMax.setText("Umidade Máxima:");
+        getContentPane().add(lblUmiMax, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 270, -1, -1));
+
+        nomePlantaField.setBackground(new java.awt.Color(204, 204, 204));
+        getContentPane().add(nomePlantaField, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 97, 250, -1));
+
+        lumensField.setBackground(new java.awt.Color(204, 204, 204));
         lumensField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 lumensFieldActionPerformed(evt);
             }
         });
-        jPanel1.add(lumensField);
-        lumensField.setBounds(370, 340, 30, 22);
-        jPanel1.add(temperaturaMaxField);
-        temperaturaMaxField.setBounds(370, 150, 30, 22);
-        jPanel1.add(temperaturaMinField);
-        temperaturaMinField.setBounds(800, 150, 30, 22);
-        jPanel1.add(umidadeMaxField);
-        umidadeMaxField.setBounds(370, 250, 30, 22);
-        jPanel1.add(umidadeMinField);
-        umidadeMinField.setBounds(800, 260, 30, 22);
+        getContentPane().add(lumensField, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 360, 40, -1));
 
+        salvarButton.setBackground(new java.awt.Color(94, 219, 18));
+        salvarButton.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        salvarButton.setForeground(new java.awt.Color(255, 255, 255));
         salvarButton.setText("SALVAR");
         salvarButton.setMaximumSize(new java.awt.Dimension(900, 500));
         salvarButton.setMinimumSize(new java.awt.Dimension(900, 500));
-        jPanel1.add(salvarButton);
-        salvarButton.setBounds(100, 400, 72, 22);
-        jPanel1.add(sliderQuantLumens);
-        sliderQuantLumens.setBounds(150, 340, 200, 20);
-        jPanel1.add(sliderTempMaxima);
-        sliderTempMaxima.setBounds(150, 150, 200, 20);
-        jPanel1.add(sliderTempMinima);
-        sliderTempMinima.setBounds(580, 150, 200, 20);
-        jPanel1.add(sliderUmiMaxima);
-        sliderUmiMaxima.setBounds(150, 250, 200, 20);
-        jPanel1.add(sliderUmiMinima);
-        sliderUmiMinima.setBounds(580, 260, 200, 20);
+        salvarButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                salvarButtonActionPerformed(evt);
+            }
+        });
+        getContentPane().add(salvarButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 420, 90, 30));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 876, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(26, 26, 26)
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(15, 15, 15))
-        );
+        lblQtdLumens.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        lblQtdLumens.setForeground(new java.awt.Color(255, 255, 255));
+        lblQtdLumens.setText("Quantidade Lumens:");
+        getContentPane().add(lblQtdLumens, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 360, -1, -1));
+
+        lblNameEstufa.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        lblNameEstufa.setForeground(new java.awt.Color(255, 255, 255));
+        lblNameEstufa.setText("Nome da Estufa:");
+        getContentPane().add(lblNameEstufa, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
+
+        lblNamePlanta.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        lblNamePlanta.setForeground(new java.awt.Color(255, 255, 255));
+        lblNamePlanta.setText("Nome da Planta:");
+        getContentPane().add(lblNamePlanta, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 100, -1, -1));
+
+        umidadeMaxField.setBackground(new java.awt.Color(204, 204, 204));
+        getContentPane().add(umidadeMaxField, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 270, 40, -1));
+
+        umidadeMinField.setBackground(new java.awt.Color(204, 204, 204));
+        getContentPane().add(umidadeMinField, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 270, 40, -1));
+
+        sliderTempMinima.setForeground(new java.awt.Color(94, 219, 18));
+        getContentPane().add(sliderTempMinima, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 180, -1, -1));
+
+        temperaturaMinField.setBackground(new java.awt.Color(204, 204, 204));
+        getContentPane().add(temperaturaMinField, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 180, 40, -1));
+
+        lblUmiMin.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        lblUmiMin.setForeground(new java.awt.Color(255, 255, 255));
+        lblUmiMin.setText("Umidade Mínima:");
+        getContentPane().add(lblUmiMin, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 270, -1, -1));
+
+        temperaturaMaxField.setBackground(new java.awt.Color(204, 204, 204));
+        getContentPane().add(temperaturaMaxField, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 180, 40, -1));
+
+        sliderTempMaxima.setForeground(new java.awt.Color(94, 219, 18));
+        getContentPane().add(sliderTempMaxima, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 180, -1, -1));
+
+        lblTempMin.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        lblTempMin.setForeground(new java.awt.Color(255, 255, 255));
+        lblTempMin.setText("Temperatura Mínima:");
+        getContentPane().add(lblTempMin, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 180, -1, -1));
+
+        sliderUmiMaxima.setForeground(new java.awt.Color(94, 219, 18));
+        getContentPane().add(sliderUmiMaxima, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 270, -1, -1));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/telaCadastroEstufa.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 914, 514));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void nomeEstufaFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nomeEstufaFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_nomeEstufaFieldActionPerformed
+
     private void lumensFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lumensFieldActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_lumensFieldActionPerformed
 
-    private void nomePlantaFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nomePlantaFieldActionPerformed
+    private void salvarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salvarButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_nomePlantaFieldActionPerformed
+    }//GEN-LAST:event_salvarButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -198,7 +209,7 @@ public class CadastroView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel lblNameEstufa;
     private javax.swing.JLabel lblNamePlanta;
     private javax.swing.JLabel lblQtdLumens;
